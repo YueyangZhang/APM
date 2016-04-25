b33e9b720f58016a5c5a2e78d3a4731aa4928506
YueyangZhang <billzyy@gmail.com>
2016-04-25 14:55:25 -0400

    templateUrl

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,18 @@
 import {Component} from "angular2/core";
+import {ProductListComponent} from "./products/product-list.component";
+import classElement = ts.ScriptElementKind.classElement;
 
 @Component({
     selector: 'pm-app',
     template: `
     <div><h1>{{pageTitle}}</h1>
-        <div>My First Component</div>
+        <pm-products></pm-products>
     </div>
-    `
+    `,
+    directives: [ProductListComponent]
 })
 
 export class AppComponent {
     pageTitle: string = 'Acme Product Management';
 }
+
--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
-1) Open a command prompt in the project's root directory (APM - Start)
+**This is practise of an Angular2 course**
+
+1) Open a command prompt in the project's root directory
 
 2) Type: `npm install`
     This installs the dependencies as defined in the package.json file. Maybe need to use npm version 2 to install latest angular2.
