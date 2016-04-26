0560031dc34fc8e36e87684551c2470bd777c6b8
YueyangZhang <billzyy@gmail.com>
2016-04-26 10:56:24 -0400

    Interfaces, Style, Custom Pipe

---
+++ app/products/product.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Bill on 4/26/2016.
+ */
+export interface IProduct {
+    productId: number;
+    productName: string;
+    productCode: string;
+    releaseDate: string;
+    price: number;
+    description: string;
+    starRating: number;
+    imageUrl: string;
+}
+
